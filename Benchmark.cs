931ae04dc0c5bf1f26ad629c04a3091c936a7594
Viktor Svyatokha <ahydrax@gmail.com>
2021-08-25 03:45:29 +1000

    WIP

---
+++ Benchmark.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace StringCtorSpanFill
+{
+    public class Benchmark
+    {
+        [Params(1, 2, 4, 5, 8, 10, 11, 12, 16, 100, 103, 128)]
+        //[Params(100)]
+        public int Count;
+
+
+        [Benchmark]
+        public string CreateNewString() => new('j', Count);
+    }
+}
